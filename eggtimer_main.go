df5027783baabd060b4623d7fff0cfaa6a344195
TSC <gst659@gmail.com>
2018-12-02 15:34:38 -0800

    Refactored out the regexp definition.

--- eggtimer_main.go
+++ eggtimer_main.go
@@ -3,44 +3,16 @@ package main
 import (
 	"fmt"
 	"os/exec"
-	"regexp"
 	"github.com/ghst659/eggtimer"
 )
-
-type RegexpDef struct {
-	name string
-	reStart *regexp.Regexp
-	reFinish *regexp.Regexp
-}
-
-func (d RegexpDef) TypeName() string {
-	return d.name
-}
-
-func (d RegexpDef) IsStart(line string) string {
-	matches := d.reStart.FindStringSubmatch(line)
-	if matches == nil {
-		return ""
-	}
-	return matches[1]
-}
-
-func (d RegexpDef) IsFinish(line string) string {
-	matches := d.reFinish.FindStringSubmatch(line)	
-	if matches == nil {
-		return ""
-	}
-	return matches[1]
-}
 
 func main() {
 	r := eggtimer.NewRunner(eggtimer.RealClock{})
 	c := exec.Command("/home/tsc/dda/dev/go/hourglass/bin/sandmock")
 	events := make(chan eggtimer.Event)
-	smd := RegexpDef {
-		name: "SandMock",
-		reStart: regexp.MustCompile(`^Begin\s+(\w+)`),
-		reFinish: regexp.MustCompile(`Finished\s+(\w+)`),
+	smd, err := eggtimer.NewRegexpDef("SandMock", `^Begin\s+(\w+)`, `Finished\s+(\w+)`)
+	if err != nil {
+		fmt.Printf("Error: %q", err)
 	}
 	var segmenter eggtimer.Segmenter
 	segmenter.AddDefinition(smd)
