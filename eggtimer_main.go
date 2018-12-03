b949f82c951b88472a7def9de35725dd1f1826ad
TSC <gst659@gmail.com>
2018-12-03 00:27:30 -0800

    Simplification of segmentation.

--- eggtimer_main.go
+++ eggtimer_main.go
@@ -10,12 +10,8 @@ func main() {
 	r := eggtimer.NewRunner(eggtimer.RealClock{})
 	c := exec.Command("/home/tsc/dda/dev/go/hourglass/bin/sandmock")
 	events := make(chan eggtimer.Event)
-	smd, err := eggtimer.NewRegexpDef("SandMock", `^Begin\s+(\w+)`, `Finished\s+(\w+)`)
-	if err != nil {
-		fmt.Printf("Error: %q", err)
-	}
 	var segmenter eggtimer.Segmenter
-	segmenter.AddDefinition(smd)
+	segmenter.AddDefinition(eggtimer.NewRegexpDef("SandMock", `^Begin\s+(\w+)`, `Finished\s+(\w+)`))
 	go r.Run(c, events)
 	activities, err := segmenter.Collect(events)
 	if err != nil {
